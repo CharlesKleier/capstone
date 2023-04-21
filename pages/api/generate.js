8d85cddcf4931a7c16784960c71acfa9d7387a68
CharlesKleier <cjkleier@uw.edu>
2023-04-20 21:47:47 -0700

    first attempt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,16 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
 
-// const fs = require('fs');
+const fs = require('fs');
 // const pdf = require('pdf-parse');
+// const Tesseract = require('tesseract.js');
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// const dataBuffer = fs.readFileSync(req.body.resume);
-// pdf(dataBuffer).then(data => {
-//   const pdfText = data.text;
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
@@ -32,26 +30,24 @@ export default async function (req, res) {
     return;
   }
 
-  // const dataBuffer = fs.readFileSync(req.body.resume);
-  // pdf(dataBuffer).then(data => {
-  //   const pdfText = data.text;
+  const resume = req.body.resume || '';
+  if (resume.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid pdf of your resume",
+      }
+    });
+    return;
+  }
 
-  // const resume = req.body.resume || '';
-  // if (question.trim().length === 0) {
-  //   res.status(400).json({
-  //     error: {
-  //       message: "Please enter a valid pdf of your resume",
-  //     }
-  //   });
-  //   return;
-  // }
+// davinci api
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(question), //+ pdfText,
+      prompt: generatePrompt(question, resume),
       temperature: 0.6,
-      max_tokens: 256
+      max_tokens: 500
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -70,19 +66,53 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(question) {
+// gpt-3.5-turbo
+//   try {
+//     const completion = await openai.createCompletion({
+//       id: 'chatcmpl-6p9XYPYSTTRi0xEviKjjilqrWU2Ve',
+//       object: 'chat.completion',
+//       created: 167764942,
+//       model: 'gpt-3.5-turbo',
+//       usage: {'prompt_tokens': 56, 'completion_tokens': 31, 'total_tokens': 87},
+//       choices: [
+//         {
+//         message: {
+//           role: 'assistant',
+//           content: generatePrompt(question, resume)},
+//         finish_reason: 'stop',
+//         index: 0
+//         }
+//       ]
+//     });
+//   } catch(error) {
+//         // Consider adjusting the error handling logic for your use case
+//         if (error.response) {
+//           console.error(error.response.status, error.response.data);
+//           res.status(error.response.status).json(error.response.data);
+//         } else {
+//           console.error(`Error with OpenAI API request: ${error.message}`);
+//           res.status(500).json({
+//             error: {
+//               message: 'An error occurred during your request.',
+//             }
+//           });
+//         }
+//       }
+// }
+
+
+
+
+function generatePrompt(question, resume) {
   const capitalizedQuestion =
     question[0].toUpperCase() + question.slice(1).toLowerCase();
-    return `Answer as a helpful, friendly, happy career advisor.
+  const resumePrompt =
+    resume[0].toUpperCase() + resume.slice(1).toLowerCase();
+    return `You are a career advisor named resumAI. 
+    helpful, friendly, personable 
+    Also use the resume to personalize the answer to the questions.
     
+    resume: ${resumePrompt}
     Question: ${capitalizedQuestion}
     Answer:`;
-//   return `Suggest three names for an animal that is a superhero.
-
-// Animal: Cat
-// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-// Animal: Dog
-// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-// Animal: ${capitalizedAnimal}
-// Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,26 @@ import styles from "./index.module.css";
 
 export default function Home() {
   const [questionInput, setQuestionInput] = useState("");
-  // const [uploadedPDF, setPDFInput] = useState("")
+  const [uploadedPDF, setPDFInput] = useState("")
   const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+    setLoading(true);
     try {
+
+      const formData = new FormData();
+      formData.append("question", questionInput);
+      formData.append("resume", uploadedPDF);
+
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ question: questionInput }),
-        // body: JSON.stringify({ resume: uploadedPDF}),
+        body: JSON.stringify({ question: questionInput, resume: uploadedPDF }),
+        
       });
 
       const data = await response.json();
@@ -26,10 +33,12 @@ export default function Home() {
 
       setResult(data.result);
       setQuestionInput("");
+      setLoading(false);
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+      setLoading(false);
     }
   }
 
@@ -44,24 +53,34 @@ export default function Home() {
         <img src="/logo.png" className={styles.icon} />
         <h3>Here to help you with your career!</h3>
         <form onSubmit={onSubmit}>
+          <p>Paste your resume from LinkedIn below:</p>
+          <input 
+            className={styles.resumeInput}
+            type="text"
+            name="resume"
+            placeholder="Paste resume here"
+            value={uploadedPDF}
+            onChange={(e) => setPDFInput(e.target.value)}
+            />
+          <p>Ask for any career guidance below:</p>
           <input
+            className={styles.questionBox}
             type="text"
             name="question"
-            placeholder="Enter Question Here"
+            placeholder="Enter question here"
             value={questionInput}
             onChange={(e) => setQuestionInput(e.target.value)}
           />
-          {/* <input 
-            type="file"
-            name="resume"
-            placeholder="upload pdf here"
-            value={uploadedPDF}
-            onChange={(e) => setPDFInput(e.target.value)}
-            /> */}
-          <input type="submit" value="Get answer to your question" />
+          <input type="submit" value="Get Response!" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.responseBox}>
+          <div className={styles.result}>
+            <div className={styles.resumAI}>ResumAI:</div>
+            {result}
+          </div>
+        </div>
       </main>
     </div>
   );
 }
+
